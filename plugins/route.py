4fe688094dfe1574e5084661faf764b6756a3439
Rajkirat Singh <itsraj3600@gmail.com>
2025-06-23 20:07:17 -0400

    Update support link in banned.py and p_ttishow.py; modify root route response in route.py

--- plugins/route.py
+++ plugins/route.py
@@ -19,7 +19,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("BenFilterBot")
+    return web.json_response("Kirat's CINE3600 Bot is running", status=200)
 
 
 @routes.get(r"/watch/{path:\S+}", allow_head=True)
