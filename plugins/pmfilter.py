b8b1b52b7e46b9cde1adcc8fb0578ec45de28788
Rajkirat Singh <rajkiratsingh555@gmail.com>
2026-07-03 18:29:24 -0400

    modified:   plugins/pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -805,6 +805,7 @@ async def filter_seasons_cb_handler(client: Client, query: CallbackQuery):
                 
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
+    logger.warning(f"CALLBACK RECEIVED: {query.data}")
     lazyData = query.data
     try:
         link = await client.create_chat_invite_link(int(REQST_CHANNEL))
